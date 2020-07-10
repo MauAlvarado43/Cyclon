bc4e65723fa59560bab58a8dac76fb5b7faa4129
Mauricio Alvarado <mawiwi201@gmail.com>
2020-07-10 15:53:09 -0500

    Database exported

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,7 @@
 import mongoose from 'mongoose'
 
-// mongoose.set('useFindAndModify', false);
-// mongoose.connect('mongodb://localhost:27017/cyclon', {
-//   useUnifiedTopology: true,  useNewUrlParser: true 
-// }).then(db => console.log('DB is connected')).catch(err => console.log(err));
-
 mongoose.set('useFindAndModify', false)
-mongoose.connect('mongodb+srv://CyC1:kybmim-hoqhob-2Doswo@cyc1-p6vhd.mongodb.net/cyclon?retryWrites=true&w=majority', 
+mongoose.connect(`${process.env.MONGO_URL}`, 
   {
     useUnifiedTopology: true,  
     useNewUrlParser: true 
