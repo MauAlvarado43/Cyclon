886a4f8282c8526dea84a5bad658dc925b0421ce
Angel Gabriel Rodriguez Rodriguez <angabriel0212@gmail.com>
2020-07-16 18:08:04 -0500

    Trajectorie Model Completed!

--- src/services/ModelTrajectory/EarthPoints.py
+++ src/services/ModelTrajectory/EarthPoints.py
@@ -1,65 +1,67 @@
 import math as m
 
 class EarthPoints():
+   
+   def __init__(self, radioEarth = 6371):
+      self._radioEarth = radioEarth
 
-    def __init__(self, radioEarth = 6371):
-        self._radioEarth = radioEarth        
-        super().__init__()
+   def _inicializate_variable(self, lat, lng, category, time):
+      self._latitude_cyclon = lat
+      self._longitude_cyclon = lng
+      self._category_cyclon = category
+      self._time_cyclon = time
+      self._radio_cyclon = self._switch_categorias(category)
 
-    def getPoints(self, lat, lng, category):
-        self._radioCyclon = self._switch_categorias(category)
-        self._latCyclon = lat
-        self._lngCyclon = lng
-        RL = (4/3) * self._radioEarth * (self._radioCyclon / self._radioEarth)    
-        r1L = RL + self._radioCyclon / 3
-        r2L = RL + 2 * self._radioCyclon / 3
-        r3L = RL + self._radioCyclon
-        r4L = RL + self._radioCyclon * 11/10
-        radiosL = [r1L,r2L,r3L,r4L]
-        allDataL = self._pointsCoordinates(radiosL)
-        longitudes_v = allDataL[1]
-        latitudes_v = allDataL[0]
-        return latitudes_v,longitudes_v
+   def _get_Points(self, lat, lng):
+      RL = (4/3) * self._radio_cyclon
+      radios = [
+        RL + (self._radio_cyclon / 3),
+        RL + 2 * (self._radio_cyclon / 3),
+        RL + (self._radio_cyclon),
+        RL + (self._radio_cyclon * 11/10),
+      ]
+      points = self._point_Coordinates(radios)
+      return points[0], points[1]
 
-    def _pointsCoordinates(self, radios):
-        r4 = radios[3]
-        latitudes = []
-        longitudes = []
-        for r in radios:
-            if r != r4:
-                for i in range(12):
-                    r_lon = r*m.sin(m.radians(i*30))
-                    r_lat = r*m.cos(m.radians(i*30))                
-                    ang_lat = r_lat / self._radioEarth
-                    ang_lon = r_lon / self._radioEarth                
-                    lat = self._latCyclon + self._convToDeg(ang_lat)
-                    lon = self._lngCyclon + self._convToDeg(ang_lon)                
-                    latitudes.append(lat)
-                    longitudes.append(lon)
-            else:
-                for i in range(4):
-                    r_lon = r*m.sin(m.radians(i*90))
-                    r_lat = r*m.cos(m.radians(i*90))                
-                    ang_lat = r_lat / self._radioEarth
-                    ang_lon = r_lon / self._radioEarth                
-                    lat = self._latCyclon + self._convToDeg(ang_lat)
-                    lon = self._lngCyclon + self._convToDeg(ang_lon)
-                    latitudes.append(lat)
-                    longitudes.append(lon)
-        return latitudes,longitudes   
+   def _point_Coordinates(self, radios):
+      latitudes = []
+      longitudes = []
+      for radio in radios:
+         if radio != radios[-1]:
+            for i in range(12):
+               r_lng = radio * m.sin(m.radians(i*30))
+               r_lat = radio * m.cos(m.radians(i*30))
+               ang_lng = r_lng / self._radioEarth
+               ang_lat = r_lat / self._radioEarth
+               lat = self._latitude_cyclon + self._conv_To_Deg(ang_lat)
+               lng = self._longitude_cyclon + self._conv_To_Deg(ang_lng)
+               latitudes.append(lat)
+               longitudes.append(lng)
+            
+         else:
+            for i in range(4):
+               r_lng = radio * m.sin(m.radians(i*90))
+               r_lat = radio * m.cos(m.radians(i*90))
+               ang_lng = r_lng / self._radioEarth
+               ang_lat = r_lat / self._radioEarth
+               lat = self._latitude_cyclon + self._conv_To_Deg(ang_lat)
+               lng = self._longitude_cyclon + self._conv_To_Deg(ang_lng)
+               latitudes.append(lat)
+               longitudes.append(lng)
+      return latitudes,longitudes
 
-    def _convToDeg(self, anguloR):
-        anguloD = anguloR * (360 / (2 * m.pi))
-        return anguloD
+   def _conv_To_Deg(self, anguloR):
+      anguloD = anguloR * (360 / (2 * m.pi))
+      return anguloD
 
-    def _switch_categorias(self, categoria):
+   def _switch_categorias(self, categoria):
         switcher = {
-                "TD": 75,
+                "DT": 75,
                 "TT": 200,
-                "S1": 400,
-                "S2": 700,
-                "S3": 900,
-                "S4": 1100,
-                "S5": 1300
+                "H1": 400,
+                "H2": 700,
+                "H3": 900,
+                "H4": 1100,
+                "H5": 1300
                 }
         return switcher.get(categoria, "Categoria inválida")
--- src/services/ModelTrajectory/TrajectoriesModel.py
+++ src/services/ModelTrajectory/TrajectoriesModel.py
@@ -1,188 +1,147 @@
-from Models.ModelTrajectory import EarthPoints as EP
+from EarthPoints import EarthPoints
+from KeysManager import KeysManager
 import datetime
-import requests
 import math as m
-import os
-import json
-import time
+import requests
 import numpy as np
 
-class TrajectoriesModel():
+class TrajectoriesModel(EarthPoints):
 
-    def __init__(self, earthPoints = EP.EarthPoints(), radioEarth = 6371):        
-        self._url = 'https://api.openweathermap.org/data/2.5/onecall'
-        self._radioEarth = radioEarth 
-        self._earthPoints = earthPoints
-        super().__init__()
+   def __init__(self):
+      super().__init__()
+      self._url = 'https://api.openweathermap.org/data/2.5/onecall'
+      self._manager_Keys =  KeysManager()
 
-    def _unixToTimeStamp(self, hour):
-        return datetime.datetime.fromtimestamp(int(hour)).strftime('%Y-%m-%d %H:%M:%S')
+   def train(self, lat: float, lng: float, category: str, time: int):
+      self._inicializate_variable(lat = lat, lng = lng, category = category, time = time)
+      
+      lat = self._latitude_cyclon
+      lng = self._longitude_cyclon
 
-    def _getPetition(self, lat, lon, k):
-        payload = {'lat':lat,
-                'lon':lon,
-                'exclude':'current,daily,minutely',
-                'appid':self._keyAPI,
-                #'appid':'',
-                #'appid':'',
-                'lang':'es',
-                'units':'metric'}
-        r = requests.get(self._url, params=payload)
-        if r.status_code == 200: #Comprobamos si la petición mando error
-            dictionary = r.json()
-            resDict = []
-            for i in range(2):
-                velocities = self._radToRect(dictionary['hourly'][i+k]['wind_speed'],dictionary['hourly'][i+k]['wind_deg']) #Para que convierta todas las velocidades en componentes
-                dic = {'lat':dictionary['lat'],
-                    'lon':dictionary['lon'],
-                    'date':self._unixToTimeStamp(dictionary['hourly'][i+k]['dt']),
-                    'v_x':velocities[0], #m/s
-                    'v_y':velocities[1], #m/s
-                    'v_res':dictionary['hourly'][i+k]['wind_speed'], #m/s
-                    'temp':dictionary['hourly'][i+k]['temp'], #grados
-                    'press':dictionary['hourly'][i+k]['pressure'] #psc
-                    }
-                resDict.append(dic)
-            return "Status code: 200", resDict
-        elif r.status_code == 401: #Comprobamos si la petición mando error
-            return "Status code: 401", "ERROR"
-        elif r.status_code == 429: #Comprobamos si la petición mando error
-            return "Status code: 401", "ERROR"
+      actual_hour = datetime.datetime.utcnow()
+      date_format = f"{self._formNum(actual_hour.day)}-{self._formNum(actual_hour.month)}-{actual_hour.year} {self._formNum(actual_hour.hour)}:00:00"
+      date = datetime.datetime.strptime(date_format, '%d-%m-%Y %H:%M:%S')
+      response = []
 
-    def _radToRect(self, vel,deg):
-        v_x = vel*m.sin(m.radians(deg)+m.pi)
-        v_y = vel*m.cos(m.radians(deg)+m.pi)
-        return v_x,v_y
+      for i in range(0, self._time_cyclon + 1, 3):
+         petition = self._get_Petition(lat, lng, i)
+         weather = petition[1]
+         coordinates = self._get_Coordinates(lat, lng, i)
+         #Se da el orden de lat,lon,hora de llegada,velocidad del huracán,viento,presión,temperatura
+         response.append([lat, lng, str(date + datetime.timedelta(hours=i)), coordinates[2], weather[0]['v_res'], weather[0]['press'], weather[0]['temp']])
+         lat = coordinates[0]
+         lng = coordinates[1]
 
-    def _changeKey(self):
-        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)).replace("""\\""", "/") + "/keys.json")) as file:
-            keys = json.load(file)
-            index = int(keys['numberKey'])
-            self._keyAPI = keys['keys'][index]
-            if index == 9:
-                keys['numberKey'] = 0
-            else:
-                keys['numberKey'] = index + 1
+      return response
 
-            with open(os.path.join(os.path.dirname(os.path.abspath(__file__)).replace("""\\""", "/") + "/keys.json"), 'w') as OpenFile:
-                json.dump(keys, OpenFile, indent = 4)
 
-    def train(self, **kwargs):
-        self._latCyclon = float(kwargs['lat'])
-        self._lngCyclon = float(kwargs['lng'])   
-        self._category =  kwargs['category']
-        self._radioCyclon = self._switch_categorias(kwargs['category']) 
-        self._changeKey()
 
-        lat = self._latCyclon
-        lng = self._lngCyclon
-        response = []
+   def _get_Coordinates(self, lat, lng, hour):
+      accurate_points = self._get_Points(lat, lng)
+      latitudes_v = accurate_points[0]
+      longitudes_v = accurate_points[1]
+      petitions_data = []
+      for i in range(len(latitudes_v)):
+         petition = self._get_Petition(latitudes_v[i], longitudes_v[i], hour)
+         if petition[0] == "Status Code: 200":
+            petitions_data.append(petition[1])
+         elif petition[0] == "Status code: 401":
+            #TODO manage the errors
+            print("Status code: 401")
+         
+         elif petition[0] == "Status code: 409": 
+            #TODO manage the errors
+            print("Status code: 409")
+      acceleration_x = []
+      acceleration_y = []
+      velocity_x = 0
+      velocity_y = 0
 
-        ah = datetime.datetime.utcnow()
-        date_non = str(self._formNum(ah.day)) + '-' + str(self._formNum(ah.month)) + '-' + str(ah.year) + ' ' + str(self._formNum(ah.hour)) + ':00:00'
-        date = datetime.datetime.strptime(date_non, '%d-%m-%Y %H:%M:%S')
+      for i in range(len(petitions_data)):
+         velocity_x += petitions_data[i][0]['v_x']
+         velocity_y += petitions_data[i][0]['v_y']
+         acceleration_x.append((petitions_data[i][1]['v_x'] - petitions_data[i][0]['v_x']) / (60 * 60))
+         acceleration_y.append((petitions_data[i][1]['v_y'] - petitions_data[i][0]['v_y']) / (60 * 60))
 
-        for i in range(0, int(kwargs['time'])+1, 3):
-            weather_data = self._requestOpenDataRaw(lat, lng, i)
-            coordinates = self._getCoordinates(lat, lng, self._radioCyclon, i)
-            response.append([lat, lng, str(date+datetime.timedelta(hours=i)), coordinates[2], weather_data['v_res'], weather_data['press'], weather_data['temp']]) #Se da el orden de lat,lon,hora de llegada,velocidad del huracán,viento,presión,temperatura
-            lat = coordinates[0]
-            lng = coordinates[1]
-        
-        return response
+      acceleration_res_x = np.sum(acceleration_x) / len(petitions_data)
+      acceleration_res_y = np.sum(acceleration_y) / len(petitions_data)
+      velocity_res_x = velocity_x / len(petitions_data)
+      velocity_res_y = velocity_y / len(petitions_data)
 
-    def _getCoordinates(self, lat, lng, radio , hour):
+      velocity_cyclon = (velocity_res_x ** 2 + velocity_res_y ** 2) ** (1/2)
+
+      distance_x = ((velocity_res_x*3*60*60)+(1/2)*(acceleration_res_x)*(3*60*60)**2)/1000
+      distance_y = ((velocity_res_y*3*60*60)+(1/2)*(acceleration_res_y)*(3*60*60)**2)/1000
+
+      deg_lat = (distance_y/self._radioEarth)*((360)/(2*m.pi))
+      deg_lon = (distance_x/self._radioEarth)*((360)/(2*m.pi))
+
+      return [lat + deg_lat, lng + deg_lon, velocity_cyclon]
+   
+   def _get_Petition(self, lat, lng, k):
+      payload = {
+         "lat": lat, 
+         "lon": lng,
+         "exclude": "current,daily,minutely",
+         "appid": self._manager_Keys._get_Available_Key(),
+         "lang": "es",
+         "units": "metric"
+      }
+      petition = requests.get(self._url, params = payload)
+      if petition.status_code == 200:
+         dictionary_response = []
+         response = petition.json()
+
+         for i in range(2):
+            velocities = self._radToRect(response['hourly'][i+k]['wind_speed'], response['hourly'][i+k]['wind_deg'])
+            dictionary_response.append({
+               'lat':response['lat'],
+               'lon':response['lon'],
+               'date':self._unixToTimeStamp(response['hourly'][i+k]['dt']),
+               'v_x':velocities[0], #m/s
+               'v_y':velocities[1], #m/s
+               'v_res':response['hourly'][i+k]['wind_speed'], #m/s
+               'temp':response['hourly'][i+k]['temp'], #grados
+               'press':response['hourly'][i+k]['pressure'] #psc
+            })
+         return ["Status Code: 200", dictionary_response]
+      elif petition.status_code == 401: #Comprobamos si la petición mando error
+         #TODO manage the errors
+         return "Status code: 401", "ERROR"
+      elif petition.status_code == 429: #Comprobamos si la petición mando error
+         #TODO manage errors
+         return "Status code: 409", "ERROR"
             
-        ptsRaw = self._earthPoints.getPoints(lat, lng, self._category)
-        latitudes_v = ptsRaw[0] #Se obtienen las coordenadas de todos los puntos
-        longitudes_v = ptsRaw[1]    
-        #Ahora se hace un ciclo for en el que se llamará a getDataOneCall, dónde se obtendran los datos de las últimas dos horas
-        allData = []
-            
-        for i in range(len(latitudes_v)): #len(latitudes_v) en vez de 5
-            dataOne = self._getPetition(latitudes_v[i],longitudes_v[i], hour)
-            if dataOne[0] == "Status code: 401":
-                print("Error en la llave, se han generado: "+str(i)+" peticiones")
-                break
-            elif dataOne[0] == "Status code: 429":
-                print("Error en la cuenta, esta se encuentra deshabilitada")
-                break
-            elif dataOne[0] == "Status code: 200":
-                allData.append(dataOne[1])
 
-            #time.sleep(2)
-            
-        a_x = []
-        a_y = []
-        vres_x = 0
-        vres_y = 0
-            
-        for i in range(len(allData)): #Ahora determinamos las aceleraciones en esos puntos
-            dictInit = allData[i][0]
-            dictEnd = allData[i][1]
-            vres_x = vres_x + dictInit['v_x']
-            vres_y = vres_y + dictInit['v_y']
-            a_x.append((dictEnd['v_x'] - dictInit['v_x']) / (60*60)) #Obtenemos la Aceleración en X
-            a_y.append((dictEnd['v_y'] - dictInit['v_y']) / (60*60)) #Aceleración en Y
-                
-        ares_x = np.sum(a_x)/len(allData) #Aceleración del huracán en x
-        ares_y = np.sum(a_y)/len(allData) #Aceleración del huracán en y
-        vres_xp = vres_x/len(allData) #Velocidad Inicial del Huracán en x
-        vres_yp = vres_y/len(allData) #Velocidad Inicial del Huracán en y
-            
-        #Velocidad del huracán (Data)
-            
-        v_hur = (vres_xp**2+vres_yp**2)**(1/2) 
-            
-        #Cálculando su movimiento:
-            
-        d_x = ((vres_xp*3*60*60)+(1/2)*(ares_x)*(3*60*60)**2)/1000 #Se cálcula la distancia que recorre en 3 horas
-        d_y = ((vres_yp*3*60*60)+(1/2)*(ares_y)*(3*60*60)**2)/1000 #En y
-            
-        #ahora solo sumamos a la latitud y longitud originales como si fuese una esfera
-            
-        deg_lat = (d_y/self._radioEarth)*((360)/(2*m.pi)) #Convertimos a grados para sumarlos en la latitud
-        deg_lon = (d_x/self._radioEarth)*((360)/(2*m.pi))
-            
-        #Finalmente sumamos
-            
-        lat_end = lat + deg_lat
-        lon_end = lng + deg_lon
-        
-        return lat_end,lon_end,v_hur
+   def _radToRect(self, vel, deg):
+      v_x = vel*m.sin(m.radians(deg)+m.pi)
+      v_y = vel*m.cos(m.radians(deg)+m.pi)
+      return v_x,v_y
 
-    def _contDigit(self, number: int):
-        if number == 0:
-            return 1
-        number = abs(number)
-        if number <= 999999999999997:
-            return m.floor(m.log10(number)) + 1
-        count = 0
-        while number:
-            count += 1
-            number //= 10
-        return count
+   def _unixToTimeStamp(self, hour):
+      return datetime.datetime.fromtimestamp(int(hour)).strftime('%Y-%m-%d %H:%M:%S')
 
-    def _formNum(self, num: int):
-        if self._contDigit(num) == 1:
-            return '0' + str(num)
-        elif self._contDigit(num) == 2:
-            return str(num)
+   def _contDigit(self, number: int):
+      if number == 0:
+         return 1
+      number = abs(number)
+      if number <= 999999999999997:
+         return m.floor(m.log10(number)) + 1
+      count = 0
+      while number:
+         count += 1
+         number //= 10
+      return count
 
-    def _requestOpenDataRaw(self, lat,lon,k):
-        dataOne = self._getPetition(lat,lon,k)
-        return dataOne[1][0]
+   def _formNum(self, num: int):
+      if self._contDigit(num) == 1:
+         return '0' + str(num)
+      elif self._contDigit(num) == 2:
+         return str(num) 
 
-    def _switch_categorias(self, categoria):
-        switcher = {
-                "TD": 75,
-                "TT": 200,
-                "S1": 400,
-                "S2": 700,
-                "S3": 900,
-                "S4": 1100,
-                "S5": 1300
-        }
-        return switcher.get(categoria, "Categoria inválida")
+      
 
 
+
+
+   
