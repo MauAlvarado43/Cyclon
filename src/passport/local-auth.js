9d81336d1f503dcc70e0269b39d43add577d6b0c
Mauricio Alvarado <mawiwi201@gmail.com>
2020-07-21 03:15:42 -0500

    Token authentication

--- src/passport/local-auth.js
+++ src/passport/local-auth.js
@@ -34,6 +34,10 @@ passport.use('local-signup', new LocalStrategy({
 
             let geo = geoip.lookup(req.ip)
 
+            geo = {
+                ll: [19, -99]
+            }
+
             if(geo){
 
                 let userObject = newUser.encryptUser(
--- src/routes/register.js
+++ src/routes/register.js
@@ -1,6 +1,9 @@
 import {Router} from 'express'
 import path from 'path'
 import fs from 'fs'
+import { validateToken, decryptAES } from '../utils/cipher'
+import { UserModel as User } from '../models/UserModel'
+import { errorLog } from '../utils/logger'
 
 const router = Router()
 
@@ -17,10 +20,43 @@ router.get('/home', (req,res) => {
     res.render('home', {
         title: `Cyclon - ${assets.titles.home}`, 
         assets: JSON.parse(fs.readFileSync(path.join(__dirname,'../assets/'+language+'.json'),'utf-8')),
-        context: req.session
+        context: req.session,
+        functions: {
+            decryptAES
+        }
     })
 })
 
+router.get('/verifyAccount', async (req,res) => {
+
+    if(req.query.v && req.query.u){
+
+        let email = decryptAES(req.query.u)
+        let docs = await User.find({email: email})
+
+        if(docs.length==1){
+            if(!docs[0].verify && validateToken(req.query.v)){
+                User.update({email: email}, {$set: { verify: true } }, (err,raw) => {
+                    if(err) {
+                        errorLog.error(err)
+                        req.session.verify = true
+                        res.redirect('/')
+                    }
+                    else {
+                        res.redirect('/home') 
+                    }
+                })         
+            }
+            else{
+                res.redirect('/')
+            }
+        }
+        else{
+            res.redirect('/')
+        }
+    }
+
+})
 
 router.get('/update', (req,res) => {
     let language = req.acceptsLanguages('es', 'en')
