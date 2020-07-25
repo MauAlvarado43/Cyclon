022c887b4e8857222291e691f142fe034fae3c95
Angel Gabriel Rodriguez Rodriguez <angabriel0212@gmail.com>
2020-07-25 01:05:50 -0500

    Python logic implemented

--- src/services/noaaSocket.py
+++ src/services/noaaSocket.py
@@ -1,5 +1,6 @@
 from ModelTrajectory.TrajectoriesModel import TrajectoriesModel
 import time
+from threading import Thread
 import requests
 import json
 import sys
@@ -8,64 +9,70 @@ import gc
 import asyncio
 import pymongo
 
-class NoaaSocket(): 
+class NoaaSocket(Thread): 
 
-    def __init__(self,):
+    def __init__(self, sio):
+        super(NoaaSocket, self).__init__()
         self._url = "https://www.nhc.noaa.gov/CurrentStorms.json"
-        self._mongoURL = "mongodb+srv://CyC1:kybmim-hoqhob-2Doswo@cyc1-p6vhd.mongodb.net/cyclon?retryWrites=true&w=majority"
+        # self._mongoURL = "mongodb+srv://CyC1:kybmim-hoqhob-2Doswo@cyc1-p6vhd.mongodb.net/cyclon?retryWrites=true&w=majority"
+        self._mongoURL = "mongodb://localhost:27017/"
         self._connect_bd()
         self._model = TrajectoriesModel()
-        self._time = 24
+        self._time = 6
+        self._sio = sio
+        self._state = True
+        self._delay = 2
+        self._json_history = {}
+        self._json_data = []
+        print("Monitoring NOAA inicializated...")
+
+    def stop(self):
+        self._state = False
 
     def _connect_bd(self):
-        client = pymongo.MongoClient(self._mongoURL)
-        database = client.cyclon
-        self._cyclone = database.hurricaines
+        try:
+            self._client = pymongo.MongoClient(self._mongo_URL)
+            self._database = self._client.cyclon
+            self._cyclone = self._database.hurricaines
+        except Exception as err:
+            print(f"Error to connect with {self._mongo_URL}")
+            self._client = pymongo.MongoClient(self._mongoLocal)
+            self._database = self._client.cyclon
+            self._cyclone = self._database.hurricaines
+            print(f"Connect with local database in {self._mongoLocal}")
 
     async def _emitAlert(self, json):
         await self._sio.emit("/alert", json)
 
     def _closeCyclone(self, id):
         self._connect_bd()
-        self._cyclone.update_one({"id": id}, {"$set": {"active": 'False'}})
+        self._cyclone.update_one({"id": id}, {"$set": {"active": False}})
 
     def _train_model(self, data):
+        print("entrenando modelo")
         self._model.save_cyclone(data)
 
-    def run(self, sio):
-
-        self._sio = sio
-
-        json_history = {}
-        json_data = []
-
-        print("Monitoring NOAA...")
-        while (True):
+    def run(self):
+        while self._state:
             try:
-                response = requests.get(self._url,headers={"Content-type":"application/json"})
+                response = requests.get(self._url,headers={"Content-type":"application/json"})                
                 if response.status_code == 200:
+                    self._json_dataTemp = json.loads(response.content)
+                    if(self._json_history != self._json_dataTemp):
+                        print("entra al if")
+                        if len(self._json_history) > 0:
+                            if len(self._json_history["activeStorms"]) > len(self._json_data):
+                                for lastCyclone in self._json_history["activeStorms"]:                               
+                                    exist = False
+                                    for actualCyclone in self._json_data: 
+                                        if lastCyclone["id"] == actualCyclone["id"]:
+                                            exist = True
 
-                    json_dataTemp = json.loads(response.content)
+                                    if not exist:
+                                        self._closeCyclone(lastCyclone["id"])
 
-                    if(json_history != json_dataTemp):
-
-                        if len(json_history["activeStorms"]) > len(json_data):
-                            
-                            for lastCyclone in json_history["activeStorms"]:
-                                
-                                exist = False
-
-                                for actualCyclone in json_data: 
-                                    if lastCyclone["id"] == actualCyclone["id"]:
-                                        exist = True
-
-                                if not exist:
-                                    self._closeCyclone(lastCyclone["id"])
-
-                        json_history = json_dataTemp
-
-                        for alert in json_dataTemp["activeStorms"]:
-
+                        self._json_history = self._json_dataTemp
+                        for alert in self._json_dataTemp["activeStorms"]:
                             category = ""
                             speed = float(alert["movementSpeed"]) * 1.60934    #Millas por hora a kilometros por hora
                             pressure = alert["pressure"]   #mB
@@ -85,7 +92,7 @@ class NoaaSocket():
                             else:
                                 category = "H5"  #Huracan Categoria 5
 
-                            json_data.append(
+                            self._json_data.append(
                                 {
                                     "id":alert["id"],
                                     "name":alert["name"],
@@ -118,15 +125,16 @@ class NoaaSocket():
 
                             )
                         
-                        asyncio.run(self._emitAlert(json_data))     
+                        asyncio.run(self._emitAlert(self._json_data))     
                         print("New data")
-                        json_data = []
+                        self._json_data = []
 
                 else:
                     print("Conection refused")
                     sys.exit()
-            except Exception as err:
+            except ZeroDivisionError as err:
+                print(err)
                 print("REFUSED with error: {0}".format(err))
             
             gc.collect()
-            time.sleep(2)
+            time.sleep(self._delay)
