c6c85713a9502e5a8ccf2cab8d14ef9bca4b6c57
Mauricio Alvarado <mawiwi201@gmail.com>
2020-07-29 22:52:20 -0500

    Python script management

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,8 +1,41 @@
 import {Router} from 'express'
+import { spawn } from 'child_process'
 import passport from 'passport'
 import path from 'path'
 import fs from 'fs'
 
 const router = Router()
 
+let child = null
+let pythonRunning = false
+
+router.get('/api/admin/run', (req,res) => {
+    if(child == null && pythonRunning == false){
+        child =  spawn(process.env.PYTHON_ENV, [path.join(__dirname, '../services/index.py')])
+        pythonRunning = true
+
+        child.on('close', (code, signal) => {
+            console.log(
+              `child process terminated due to receipt of signal ${signal}`)
+        })
+        
+        res.send("START")
+    }
+    else{
+        res.send("START'N :v")
+    }
+})
+
+router.get('/api/admin/stop', (req,res) => {
+    if(child != null && pythonRunning == true){
+        child.kill('SIGTERM')
+        child =  null
+        pythonRunning = false
+        res.send("STOP")
+    }
+    else{
+        res.send("STOP'N :v")
+    }
+})
+  
 module.exports = router
