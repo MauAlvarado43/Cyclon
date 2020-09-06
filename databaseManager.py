816dcfa425a86285d1061672ee4e53c1cffbb5ca
Mauricio Alvarado <mawiwi201@gmail.com>
2020-09-06 17:26:01 -0500

    Rework

---
+++ databaseManager.py
@@ -0,0 +1,117 @@
+from queryOpenWeather import QueryOpenWeather
+from datetime import datetime
+from dateutil import parser
+import pymongo
+import requests
+import json
+
+class DBManager:
+
+    def __init__(self):
+        super(DBManager, self).__init__()
+        self._mongoClient = pymongo.MongoClient("mongodb+srv://CyC1:kybmim-hoqhob-2Doswo@cyc1-p6vhd.mongodb.net/cyclon?retryWrites=true&w=majority")
+        self._database = self._mongoClient["cyclon"]
+        self._model = self._database["hurricaines"]
+        self._openWeather = QueryOpenWeather()
+
+    def formatPoint(self, storm):
+
+        dataReal = self._openWeather.getRealPoint(storm["center"])
+        dataReal = [{
+            "position": {
+                "lat": storm["center"]["lat"],
+                "lng": storm["center"]["lng"]
+            },
+            "windSpeed": dataReal["wind"]["speed"],
+            "hurrSpeed": storm["data"]["hurrSpeed"],
+            "temperature": dataReal["main"]["temp"],
+            "pressure": dataReal["main"]["pressure"],
+            "date": storm["date"]
+        }]
+
+        dataPredicted = [dataReal[0]]
+
+        for point in storm["predicted"]:
+
+            for forecast in self._openWeather.getForecastPoint(point["center"])["list"]:
+
+                if(forecast["dt_txt"] == str(point["date"])):
+
+                    dataPredicted.append({
+                        "position": {
+                            "lat": point["center"]["lat"],
+                            "lng": point["center"]["lng"]
+                        },
+                        "windSpeed": forecast["wind"]["speed"],
+                        "hurrSpeed": storm["data"]["hurrSpeed"],
+                        "temperature": forecast["main"]["temp"],
+                        "pressure": forecast["main"]["pressure"],
+                        "date": point["date"]
+                    })
+
+                    break
+
+        return [dataReal, dataPredicted]
+
+    def compare(self, activeStorms):
+        docs = self._model.find({ "active" : True })
+
+        #Search old storms in new storms and stopping not actives
+        for doc in docs:
+
+            exist = False
+            stormData = {}
+
+            for storm in activeStorms:
+                
+                if storm["id"] == doc["id"]:
+
+                    exist = True
+                    stormData = storm
+
+                    break
+            
+            #Close inactive cyclone
+            if not exist:
+                self._model.update_one( { "id" : doc["id"]}, { "$set": { "active" : False } })
+                print("Cyclone closed")
+            else:
+                realTrajectory = doc["realTrajectory"]
+                
+                #Check repeated data
+                if (str(doc["lastUpdate"].isoformat()) + ".000Z") != str(storm["date"]):
+                    data = self.formatPoint(storm)
+
+                    realTrajectory = doc["realTrajectory"]
+                    realTrajectory.append(data[0][0])
+
+                    self._model.update_one( { "id" : doc["id"]}, { "$set": { "realTrajectory" : realTrajectory, "predictedTrajectory": data[1], "lastUpdate":  parser.parse(storm["date"]) } })
+
+                    print("Cyclone updated")
+
+        #Search new storms in olds and created
+        for storm in activeStorms:
+            exist = False
+            doc = self._model.find({ "id" : storm["id"] }) 
+
+            if doc.count() == 0:
+
+                data = self.formatPoint(storm)
+
+                cyclone = {
+                    "id": storm["id"],
+                    "origin": storm["center"],
+                    "name": storm["name"],
+                    "appearance": parser.parse(storm["date"]),
+                    "lastUpdate": parser.parse(storm["date"]),
+                    "active": True,
+                    "category": storm["category"],
+                    "predictedTrajectory": data[1],
+                    "realTrajectory": data[0]
+                }
+
+                self._model.insert_one(cyclone)
+
+                print("New cyclone saved")
+                
+                
--- noaaSocket.py
+++ noaaSocket.py
@@ -1,140 +1,173 @@
-from ModelTrajectory.TrajectoriesModel import TrajectoriesModel
-import time
 from threading import Thread
+from databaseManager import DBManager
+from datetime import datetime, timezone, timedelta
+from time import strptime
+import kml2geojson
+import zipfile
+import time
 import requests
 import json
+import geojson
 import sys
 import os
+import shutil
 import gc
-import asyncio
-import pymongo
 
-class NoaaSocket(Thread): 
+class NoaaSocket(Thread):
 
-    def __init__(self, sio):
+    def __init__(self):
         super(NoaaSocket, self).__init__()
+        self._cycloneManager = DBManager()
         self._url = "https://www.nhc.noaa.gov/CurrentStorms.json"
-        self._mongo_URL = "mongodb+srv://CyC1:kybmim-hoqhob-2Doswo@cyc1-p6vhd.mongodb.net/cyclon?retryWrites=true&w=majority"
-        # self._mongo_URL = "mongodb://localhost:27017/"
-        self._connect_bd()
-        self._model = TrajectoriesModel()
-        self._time = 6
-        self._sio = sio
-        self._state = True
-        self._delay = 2
         self._json_history = {}
         self._json_data = []
+        self._delay = 2
         print("Monitoring NOAA inicializated...")
 
-    def stop(self):
-        self._state = False
-
-    def _connect_bd(self):
-        try:
-            self._client = pymongo.MongoClient(self._mongo_URL)
-            self._database = self._client.cyclon
-            self._cyclone = self._database.hurricaines
-        except Exception as err:
-            print(f"Error to connect with {self._mongo_URL}")
-            self._client = pymongo.MongoClient(self._mongoLocal)
-            self._database = self._client.cyclon
-            self._cyclone = self._database.hurricaines
-            print(f"Connect with local database in {self._mongoLocal}")
-
-    async def _emitAlert(self, json):
-        await self._sio.emit("/alert", json)
-
-    def _closeCyclone(self, id):
-        self._connect_bd()
-        self._cyclone.update_one({"id": id}, {"$set": {"active": False}})
-
-    def _train_model(self, data):
-        print("entrenando modelo")
-        self._model.save_cyclone(data)
-
-    def run(self):
-        while self._state:
+    def run(self, socketio):
+        while True:
             try:
                 response = requests.get(self._url,headers={"Content-type":"application/json"})                
                 if response.status_code == 200:
+
+                    #Parse response to JSON
                     self._json_dataTemp = json.loads(response.content)
-                    if(self._json_history != self._json_dataTemp):
-                        print("entra al if")
-                        if len(self._json_history) > 0:
-                            if len(self._json_history["activeStorms"]) > len(self._json_data):
-                                for lastCyclone in self._json_history["activeStorms"]:                               
-                                    exist = False
-                                    for actualCyclone in self._json_data: 
-                                        if lastCyclone["id"] == actualCyclone["id"]:
-                                            exist = True
 
-                                    if not exist:
-                                        self._closeCyclone(lastCyclone["id"])
+                    #New data differ from old
+                    if(self._json_history != self._json_dataTemp):      
 
-                        self._json_history = self._json_dataTemp
                         for alert in self._json_dataTemp["activeStorms"]:
                             category = ""
-                            speed = float(alert["movementSpeed"]) * 1.60934    #Millas por hora a kilometros por hora
+                            speed = float(alert["movementSpeed"]) * 1.60934    #mph to km/h
                             pressure = alert["pressure"]   #mB
 
                             if speed < 62:
-                                category = "DT"  #Depresion Tropical
+                                category = "DT"  #Tropical Depression
                             elif speed < 118:
-                                category = "TT"  #Tormenta Tropical
+                                category = "TT"  #Tropical Storm
                             elif speed < 153:
-                                category = "H1"  #Huracan Categoria 1
+                                category = "H1"  #Hurricaine 1
                             elif speed < 177:
-                                category = "H2"  #Huracan Categoria 2
+                                category = "H2"  #Hurricaine 2
                             elif speed < 210:
-                                category = "H3"  #Huracan Categoria 3
+                                category = "H3"  #Hurricaine 3
                             elif speed < 250:
-                                category = "H4"  #Huracan Categoria 4
+                                category = "H4"  #Hurricaine 4
                             else:
-                                category = "H5"  #Huracan Categoria 5
+                                category = "H5"  #Hurricaine 5
 
+                            predictedTrack = []
+
+                            #Download KMZ file from NOAA and change to ZIP
+                            kmlURL = alert["forecastTrack"]["kmzFile"]
+                            file =  requests.get(kmlURL)
+                            path = os.path.join(os.path.dirname(os.path.abspath(__file__)).replace("""\\""", "/") + "/kml/"+alert["id"]+".zip")
+                            pathExtracted = os.path.join(os.path.dirname(os.path.abspath(__file__)).replace("""\\""", "/") + "/kml/"+alert["id"])
+
+                            #Write file downloaded
+                            with open(path, "wb") as f:
+                                f.write(file.content)
+                            
+                            #Extract ZIP file
+                            with zipfile.ZipFile(path, 'r') as unzip:
+                                unzip.extractall(pathExtracted)
+
+                            #Read and convert KML to GEOJSON
+                            kml2geojson.main.convert(pathExtracted + "/"+ kmlURL.split("/")[5].split(".")[0] + ".kml", pathExtracted)
+
+                            #Read GEOJSON
+                            with open(pathExtracted + "/"+ kmlURL.split("/")[5].split(".")[0] +".geojson") as file:
+                                data = geojson.load(file)
+                                trajectory = data["features"]
+
+                                #Drop indexes without date (has all coordinates and is not util)
+                                trajectory.pop(0) 
+                                trajectory.pop(0) 
+
+                                #Put in an array each point predicted by NOAA
+                                for point in trajectory:
+                                    predictedTrack.append({
+                                        "center": {
+                                            "lat": point["geometry"]["coordinates"][1],
+                                            "lng": point["geometry"]["coordinates"][0]
+                                        },
+                                        "date": self.dateConvert(point["properties"]["description"].split("<td>")[3].split("<td nowrap>")[2].replace("Valid at:", "").split("</td>")[0].strip())
+                                    })
+                                
+                            #Add storm to storm's array
                             self._json_data.append(
                                 {
-                                    "id":alert["id"],
-                                    "name":alert["name"],
-                                    "category":category,
-                                    "speed":speed,
-                                    "pressure":pressure,
-                                    "center":{
-                                        "lat":alert["latitudeNumeric"],
-                                        "long":alert["longitudeNumeric"]
-                                    },
-                                    "direction":alert["movementDir"],  #Grados
-                                    "date":alert["lastUpdate"]
-                                }
-                            )
-
-                            self._train_model(
-                                {
-                                    "id":alert["id"],
-                                    "category":category,
+                                    "id": alert["id"],
                                     "name": alert["name"],
-                                    "speed":speed,
-                                    "pressure":pressure,
-                                    "center":{
-                                        "lat":alert["latitudeNumeric"],
-                                        "long":alert["longitudeNumeric"]
+                                    "category": category,
+                                    "data": {
+                                        "hurrSpeed": speed,
                                     },
-                                    "direction":alert["movementDir"],  #Grados
-                                    "date":alert["lastUpdate"]
+                                    "center":{
+                                        "lat": alert["latitudeNumeric"],
+                                        "lng": alert["longitudeNumeric"]
+                                    },
+                                    "date": alert["lastUpdate"],
+                                    "predicted": predictedTrack
                                 }
-
                             )
                         
-                        asyncio.run(self._emitAlert(self._json_data))     
-                        print("New data")
+                        print("New data detected")
+
+                        #Emit alert
+                        socketio.emit('/alert', { "data": self._json_data } , namespace='/api')
+
+                        print("Alert emited")
+
+                        #Use the storms
+                        self._cycloneManager.compare(self._json_data)
+
+                        #Clean storms
                         self._json_data = []
+
+                        #Clear folder 
+                        folder = os.path.join(os.path.dirname(os.path.abspath(__file__)).replace("""\\""", "/") + "/kml/")
+
+                        for filename in os.listdir(folder):
+                            file_path = os.path.join(folder, filename)
+                            try:
+                                if os.path.isfile(file_path) or os.path.islink(file_path):
+                                    os.unlink(file_path)
+                                elif os.path.isdir(file_path):
+                                    shutil.rmtree(file_path)
+                            except Exception as e:
+                                print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+                    #Save the history state 
+                    self._json_history = self._json_dataTemp
 
                 else:
                     print("Conection refused")
                     sys.exit()
+
             except ZeroDivisionError as err:
                 print(err)
                 print("REFUSED with error: {0}".format(err))
             
             gc.collect()
             time.sleep(self._delay)
+
+    #Convert date given from NOAA to UTC ISO string
+    def dateConvert(self, date):
+
+        split = date.split(" ")
+
+        date = split[0] + " " + split[1] + " " + split[3] + " " + split[4] + " " + split[5]
+
+        full_date = datetime.strptime(date, '%I:%M %p %B %d, %Y')
+
+        delta = timedelta()
+
+        if split[2] == "CDT":
+            delta = timedelta(hours = 5)
+        elif split[2] == "MDT":
+            delta = timedelta(hours = 6)
+
+        dateUTC = full_date + delta
+
+        return str(dateUTC)
