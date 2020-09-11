55ef13c05e7e2500c3ffeb7c5c75e0271bdcdefe
Mauricio Alvarado <mawiwi201@gmail.com>
2020-09-11 16:25:33 -0500

    APK signed

--- src/public/js/lib/scripts.js
+++ src/public/js/lib/scripts.js
@@ -35,11 +35,9 @@
 	$(function() {
 		$(document).on('click', 'a.page-scroll', function(event) {
             var $anchor = $(this);
-            if(this.id == "" || this.id == "contactButton"){
-                $('html, body').stop().animate({
-                    scrollTop: $($anchor.attr('href')).offset().top
-                }, 600, 'easeInOutExpo');
-            }
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 600, 'easeInOutExpo');
 			event.preventDefault();
 		});
 	});
