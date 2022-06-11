223db56dd9a0135651a41c1f27852c2692e3b8a4
Mauricio Alvarado <mau.alvalop43@gmail.com>
2022-06-11 17:16:30 -0500

    Add test

--- cyclon.py
+++ cyclon.py
@@ -1,10 +1,12 @@
 from noaaSocket import NoaaSocket
+from databaseManager import DBManager
 from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 import threading
 import os
 import logging
+from datetime import datetime, timedelta
 
 
 app = Flask(__name__)
@@ -25,6 +27,97 @@ def main():
 @app.route('/api/data')
 def dataRequest():
     return socket.getAlertsGenerated()
+
+@app.route('/api/test')
+def test():
+
+    db = DBManager()
+
+    data = { 
+        "data": { 
+            "id": "ad012022",
+            "lastPoint": {
+                "position": {
+                    "lat": 16.7,
+                    "lng": -103.2
+                },
+                "windSpeed": 179.4,
+                "hurrSpeed": 22.4,
+                "temperature": 27.94,
+                "pressure": 1005,
+                "date": datetime.now().isoformat()
+            },
+            "name": "Simulation 1",
+            "category": "H1",
+            "nearest": DBManager().getNearestCity(16.7, -103.2)
+        }, 
+        "update": False 
+    }
+
+    cyclone = {
+        "id": "ad012022",
+        "origin": {
+            "lat": 16.7,
+            "lng": -102.2
+        },
+        "name": "Simulation 1",
+        "appearance": datetime.now().isoformat(),
+        "lastUpdate": datetime.now().isoformat(),
+        "active": True,
+        "category": "H1",
+        "predictedTrajectory": [
+            {
+                "position": {
+                    "lat": 16.7,
+                    "lng": -102.2
+                },
+                "windSpeed": 200.4,
+                "hurrSpeed": 24.4,
+                "temperature": 27.94,
+                "pressure": 1005,
+                "date": (datetime.now() + timedelta(hours=3)).isoformat()
+            },
+            {
+                "position": {
+                    "lat": 18.7,
+                    "lng": -104.2
+                },
+                "windSpeed": 260.4,
+                "hurrSpeed": 24.4,
+                "temperature": 27.94,
+                "pressure": 1005,
+                "date": (datetime.now() + timedelta(hours=6)).isoformat()
+            },
+            {
+                "position": {
+                    "lat": 22.7,
+                    "lng": -110.2
+                },
+                "windSpeed": 50.4,
+                "hurrSpeed": 24.4,
+                "temperature": 27.94,
+                "pressure": 1005,
+                "date": (datetime.now() + timedelta(hours=9)).isoformat()
+            }
+        ],
+        "realTrajectory": [{
+           "position": {
+                "lat": 16.7,
+                "lng": -102.2
+            },
+            "windSpeed": 179.4,
+            "hurrSpeed": 22.4,
+            "temperature": 27.94,
+            "pressure": 1005,
+            "date": datetime.now().isoformat()
+        }]
+    }
+
+    db._model.insert_one(cyclone)
+
+    socketio.emit('/alert', data, namespace='/api')
+
+    return 'Emitted'
 
 socketio = SocketIO(app, logger=False, engineio_logger=False, debug=False, cors_allowed_origins='*')
 
